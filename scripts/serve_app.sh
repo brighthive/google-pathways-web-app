4de3bb2efd2ecf05f54c0f753534cde8793bf045
Regina Compton <reginafcompton@gmail.com>
2020-04-15 16:40:28 -0400

    chore: Remove timeout for non-local environments

--- scripts/serve_app.sh
+++ scripts/serve_app.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-WORKERS=4
 
-bash scripts/migrate.sh && \
-gunicorn --workers=$WORKERS -b 0.0.0.0:8000 wsgi:app --reload --worker-class gevent --timeout 600
+bash scripts/migrate.sh
+
+if [ "$APP_ENV" == "LOCAL" ] || [ -z "$APP_ENV" ]; then
+    WORKERS=4
+    gunicorn --workers=$WORKERS -b 0.0.0.0:8000 wsgi:app --reload --worker-class gevent --timeout 600
+else
+    gunicorn -b 0.0.0.0:8000 -w $WORKERS wsgi:app --worker-class gevent
+fi
