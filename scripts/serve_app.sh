32f5a2a0acf95cc5aba75db7d36ec31561ed3b07
Regina Compton <reginafcompton@gmail.com>
2020-04-15 17:00:26 -0400

    chore: Change operator to single eq – for sh support

--- scripts/serve_app.sh
+++ scripts/serve_app.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+WORKERS=4
 
 bash scripts/migrate.sh
 
-if [ "$APP_ENV" == "LOCAL" ] || [ -z "$APP_ENV" ]; then
-    WORKERS=4
+if [ $APP_ENV = "LOCAL" ]
+then
     gunicorn --workers=$WORKERS -b 0.0.0.0:8000 wsgi:app --reload --worker-class gevent --timeout 600
 else
     gunicorn -b 0.0.0.0:8000 -w $WORKERS wsgi:app --worker-class gevent
